37d06059845553dfdbee732d1b649cdbc6f22764
Tran Van Thuc <tranvanthuc365@gmail.com>
2019-03-29 23:06:28 +0700

    Remove Ws

--- jenkins/scripts/build.sh
+++ jenkins/scripts/build.sh
@@ -4,9 +4,10 @@ cp .env.example .env
 sudo docker-compose up --build -d
 sudo chmod -R 777 storage vendor node_modules .env
 rm -rf composer.lock
-composer install
+docker exec -i web bash
 
 # in docker container web
+composer install
 php artisan key:generate
 php artisan config:clear
 php artisan migrate
