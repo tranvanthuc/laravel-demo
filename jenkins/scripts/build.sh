171446ec70f6588eda7645473ecd2859238c112d
Tran Van Thuc <thuc.tv@workhouse.me>
2019-03-29 09:40:55 +0700

    Develop

--- jenkins/scripts/build.sh
+++ jenkins/scripts/build.sh
@@ -2,12 +2,14 @@
 
 cp .env.example .env
 sudo docker-compose up -d
-sudo chmod -R 777 storage
+sudo chmod -R 777 storage vendor node_modules
 composer install
 sudo docker exec -i web bash
 
 # in docker container web
 php artisan key:generate
+php artisan cache:clear
+php artisan config:clear
 php artisan config:cache
 yarn install
 yarn dev
