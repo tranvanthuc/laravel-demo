4e8614f0fba967a744e47594f4083ab91533f80e
Tran Van Thuc <thuc.tv@workhouse.me>
2019-03-29 16:08:36 +0700

    Add

--- jenkins/scripts/build.sh
+++ jenkins/scripts/build.sh
@@ -4,10 +4,9 @@ cp .env.example .env
 sudo docker-compose up --build -d
 sudo chmod -R 777 storage vendor node_modules .env
 composer install
-sudo docker-compose exec web php artisan key:generate
-sudo docker exec -i web bash
-sleep 3
+
 # in docker container web
+php artisan key:generate
 php artisan config:clear
 php artisan migrate
 yarn install
