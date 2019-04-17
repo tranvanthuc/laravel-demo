c4edcd7e36104ec91314d21ca291fb0140b75ff8
Tran Van Thuc <tranvanthuc365@gmail.com>
2019-04-17 23:35:18 +0700

    Update build.sh

--- jenkins/scripts/build.sh
+++ jenkins/scripts/build.sh
@@ -3,10 +3,11 @@
 cp .env.example .env
 sudo docker-compose up --build -d
 sudo chmod -R 777 storage node_modules .env
+sudo rm -rf composer.lock
 sudo docker exec -i pipeline-docker_develop_web_1 bash
 
 # in docker container web
-rm -rf composer.lock
+
 composer install
 php artisan key:generate
 php artisan config:clear
