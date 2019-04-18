0b9a9fdfd3a9290cc3d5b2d53608659f54849393
Tran Van Thuc <tranvanthuc365@gmail.com>
2019-04-18 21:30:26 +0700

    Update build.sh

--- jenkins/scripts/build.sh
+++ jenkins/scripts/build.sh
@@ -4,13 +4,12 @@ cp .env.example .env
 sudo docker-compose up --build -d
 # sudo chmod -R 777 storage vendor node_modules .env
 sudo rm -rf composer.lock
-sudo docker exec -i laravel_demo_web bash
 
 # in docker container web
 
-composer install
-php artisan key:generate
-php artisan config:clear
-php artisan migrate
-yarn install
-yarn dev
+sudo docker-compose exec web composer install
+sudo docker-compose exec web php artisan key:generate
+sudo docker-compose exec web php artisan config:clear
+sudo docker-compose exec web php artisan migrate
+sudo docker-compose exec web yarn install
+sudo docker-compose exec web yarn dev
