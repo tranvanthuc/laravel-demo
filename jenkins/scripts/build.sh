df3639ba02618acc34f5416d7e53df2687b37e49
Tran Van Thuc <tranvanthuc365@gmail.com>
2019-04-18 21:00:47 +0700

    Update docker file

--- jenkins/scripts/build.sh
+++ jenkins/scripts/build.sh
@@ -4,10 +4,9 @@ cp .env.example .env
 sudo docker-compose up --build -d
 sudo chmod -R 777 storage vendor node_modules .env
 sudo rm -rf composer.lock
-sudo docker exec -i pipeline-docker_develop_web_1 bash
+sudo docker exec -i laravel_demo_web bash
 
 # in docker container web
-
 composer install
 php artisan key:generate
 php artisan config:clear
