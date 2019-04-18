496e3d38ff8f3077328cfbaa595d7351412e01aa
Tran Van Thuc <tranvanthuc365@gmail.com>
2019-04-18 21:21:16 +0700

    Update build

--- jenkins/scripts/build.sh
+++ jenkins/scripts/build.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env sh
 
 cp .env.example .env
-sudo docker-compose up --build -d
+ docker-compose up --build -d
 sudo chmod -R 777 storage vendor node_modules .env
-sudo rm -rf composer.lock
-sudo docker exec -i laravel_demo_web bash
+ rm -rf composer.lock
+ docker exec -it laravel_demo_web bash
 
 # in docker container web
+
 composer install
 php artisan key:generate
 php artisan config:clear
