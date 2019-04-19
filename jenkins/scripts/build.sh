958b0473206d3276f0daa549205472aca1b64d95
Tran Van Thuc <tranvanthuc365@gmail.com>
2019-04-19 22:03:11 +0700

    Update dockerfile

--- jenkins/scripts/build.sh
+++ jenkins/scripts/build.sh
@@ -2,8 +2,9 @@
 
 cp .env.example .env
 sudo docker-compose up --build -d
-sudo chown -R jenkins:jenkins storage bootstrap public .env
+# sudo chown -R jenkins:jenkins storage bootstrap public .env
 sudo rm -rf composer.lock
 
 # in docker container web
+sudo docker-compose exec -T app composer install
 sudo docker-compose exec -T app php artisan config:cache
