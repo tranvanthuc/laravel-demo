5b883cadf356c4bc5036ee12b68c144b31cde9ad
Tran Van Thuc <tranvanthuc365@gmail.com>
2019-04-19 22:11:43 +0700

    Update dockerfile

--- jenkins/scripts/build.sh
+++ jenkins/scripts/build.sh
@@ -1,17 +1,15 @@
 #!/usr/bin/env sh
 
-cp .env.example .env
+# cp .env.example .env
 sudo docker-compose up --build -d
-# sudo chown -R jenkins:jenkins storage bootstrap public .env
-sudo rm -rf composer.lock
 
 # in docker container web
 sudo docker-compose exec -T app composer install
 sudo docker-compose exec -T app php artisan key:generate
 sudo docker-compose exec -T app php artisan config:cache
 sudo docker-compose exec -T app php artisan migrate
-sudo docker-compose exec -T app php artisan yarn install
-sudo docker-compose exec -T app php artisan yarn dev
+sudo docker-compose exec -T app yarn install
+sudo docker-compose exec -T app yarn dev
 
 
 
